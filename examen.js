c830653d2cf234e3463ec7de348e6c4cf12ba1de
EvaCabrerizo <147143748+EvaCabrerizo@users.noreply.github.com>
2023-11-17 08:46:25 +0100

    examen

--- examen.js
+++ examen.js
@@ -1,1 +1,22 @@
-const baseUrl ="https://api.themoviedb.org/3/";
+const baseUrl = 'https://wizard-world-api.herokuapp.com/';
+
+window.onload = async() => {
+  const persons = await getAllCharacters();
+  
+
+  for (const person of persons) {
+    const mainHtmlElement = document.getElementById('main');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `
+      <h2>${person.firstName}</h2>
+      <p>${person.elixirs}</p>
+    `;
+    mainHtmlElement.appendChild(newElement);
+  }
+
+};
+async function getAllCharacters() {
+  const response = await fetch(`${baseUrl}/Wizards`);
+  const data = await response.json();
+  return data;
+}
