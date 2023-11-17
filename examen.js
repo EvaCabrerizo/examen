9d227eba53e92468a1a968db3a2593a539414e3a
EvaCabrerizo <147143748+EvaCabrerizo@users.noreply.github.com>
2023-11-17 09:52:07 +0100

    examen

--- examen.js
+++ examen.js
@@ -12,8 +12,10 @@ window.onload = async() => {
       const newElement = document.createElement('div');
       newElement.innerHTML = `
         <h2>${person.firstName} ${person.lastName}</h2>
-        <p>${person.elixirs.name}</p>
-      `;
+        <p>${person.elixirs.name}</p>`;
+        newButton.innerText = person.elixirs.name;
+        newButton.addEventListener('click', () => showCharacterDetails(person, newButton));
+        mainHtml.appendChild(newButton);
       mainHtmlElement.appendChild(newElement);
     }
   
@@ -25,3 +27,24 @@ async function getAllCharacters() {
   return data;
 }
 
+
+function showIngredients(character, button) {
+  const mainHtml = document.getElementById('main');
+  const detailsId = `details-${character.name}`;
+  const detailsHtml = document.getElementById(detailsId);
+
+  if (!detailsHtml) {
+      // Si no existe, crea un nuevo elemento details
+      const newDetails = document.createElement('div');
+      newDetails.id = detailsId;
+      newDetails.className = 'character-details';
+      newDetails.innerHTML = `
+          <p>Altura: ${elixir}</p>`;
+
+      // Agrega el nuevo elemento details debajo del botón
+      button.parentNode.insertBefore(newDetails, button.nextSibling);
+  } else {
+      // Si ya existe, elimina el elemento details
+      detailsHtml.parentNode.removeChild(detailsHtml);
+  }
+}
