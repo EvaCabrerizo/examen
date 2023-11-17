dc90d8fd8ed3c7cfc340563cd8fd13926b64a7ae
EvaCabrerizo <147143748+EvaCabrerizo@users.noreply.github.com>
2023-11-17 11:29:19 +0100

    examen

--- examen.js
+++ examen.js
@@ -13,30 +13,38 @@ window.onload = async() => {
     newElement.innerHTML = `
       <h2>${person.firstName} ${person.lastName}</h2>`
        for (const elixir of person.elixirs) {
-        
         newElement.innerHTML += `
            <p>${elixir.name}</p>
-           <button onclick="elixirClicat(${elixir.name})">Show ingredients of ${elixir.name}</button>
-         `;
-         
+           <button onclick="elixirClicat('${elixir.id}')">Show ingredients of ${elixir.name}</button>`   
+         }  
        }
 
-       
-
-      
      mainHtmlElement.appendChild(newElement);
 
-  }
-};
+  };
 
 async function getAllCharacters() {
   const response = await fetch(`${baseUrl}/Wizards`);
   const data = await response.json();
   return data;
 }
-async function getAllIngredients(elixir) {
-  const response = await fetch(`${baseUrl}/Elixirs`);
+async function getAllIngredients(id) {
+  const response = await fetch(`${baseUrl}/Elixirs/${id}`);
   const data = await response.json();
-  return data;
+  return data.ingredients;
 }
 
+function showIngredients(ingredients){
+  for (const ingredient of ingredients) {
+    const mainHtmlElement1 = document.getElementById('ingredients');
+    const newElement1 = document.createElement('div');
+    newElement1.innerHTML = `
+      <p>${ingredient}</p>`
+      mainHtmlElement1.appendChild(newElement1);
+ }
+}
+
+async function elixirClicat(id){
+  ingredients = getAllIngredients(id);
+  
+}
