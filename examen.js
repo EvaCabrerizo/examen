8a69227863cc0854ea54496eddd1a00706ea586f
EvaCabrerizo <147143748+EvaCabrerizo@users.noreply.github.com>
2023-11-17 09:19:53 +0100

    examen

--- examen.js
+++ examen.js
@@ -2,25 +2,28 @@ const baseUrl = 'https://wizard-world-api.herokuapp.com';
 
 window.onload = async() => {
   const persons = await getAllCharacters();
+  const elixirs = await getAllElixirs();
   
   const spinnerHtmlElement = document.getElementById('spinner');
   spinnerHtmlElement.remove();
 
   for (const person of persons) {
-    const mainHtmlElement = document.getElementById('main');
-    const newElement = document.createElement('div');
-    newElement.innerHTML = `
-      <h2>${person.lastName}, ${person.firstName}</h2>
-      <p>${person.elixirs}</p>
-    `;
-    mainHtmlElement.appendChild(newElement);
-  }
 
-};
+      const mainHtmlElement = document.getElementById('main');
+      const newElement = document.createElement('div');
+      newElement.innerHTML = `
+        <h2>${person.firstName} ${person.lastName}</h2>
+        <p>${}</p>
+      `;
+      mainHtmlElement.appendChild(newElement);
+    }
+  
+  };
+
 async function getAllCharacters() {
   const response = await fetch(`${baseUrl}/Wizards`);
   const data = await response.json();
-  return data;
+  return data.elixirs;
 }
 
 async function getAllElixirs() {
