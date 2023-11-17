fefaba9b3389ed18e7dbb1207e64f24d000f64b7
EvaCabrerizo <147143748+EvaCabrerizo@users.noreply.github.com>
2023-11-17 10:02:01 +0100

    ex

--- examen.js
+++ examen.js
@@ -7,17 +7,14 @@ window.onload = async() => {
   spinnerHtmlElement.remove();
 
   for (const person of persons) {
-
       const mainHtmlElement = document.getElementById('main');
       const newElement = document.createElement('div');
       newElement.innerHTML = `
         <h2>${person.firstName} ${person.lastName}</h2>
-        <p>${person.elixirs.name}</p>`;
-        newButton.innerText = person.elixirs.name;
-        newButton.addEventListener('click', () => showCharacterDetails(person, newButton));
-        mainHtml.appendChild(newButton);
-      mainHtmlElement.appendChild(newElement);
+        `;
+
     }
+    mainHtmlElement.appendChild(newElement);
   
   };
 
@@ -26,25 +23,26 @@ async function getAllCharacters() {
   const data = await response.json();
   return data;
 }
-
-
-function showIngredients(character, button) {
-  const mainHtml = document.getElementById('main');
-  const detailsId = `details-${character.name}`;
-  const detailsHtml = document.getElementById(detailsId);
-
-  if (!detailsHtml) {
-      // Si no existe, crea un nuevo elemento details
-      const newDetails = document.createElement('div');
-      newDetails.id = detailsId;
-      newDetails.className = 'character-details';
-      newDetails.innerHTML = `
-          <p>Altura: ${elixir}</p>`;
-
-      // Agrega el nuevo elemento details debajo del botón
-      button.parentNode.insertBefore(newDetails, button.nextSibling);
-  } else {
-      // Si ya existe, elimina el elemento details
-      detailsHtml.parentNode.removeChild(detailsHtml);
-  }
+async function getAllElixirs() {
+  const response = await fetch(`${baseUrl}/Elixirs`);
+  const data = await response.json();
+  return data;
 }
+
+// function showIngredients(elixir, button) {
+//   const mainHtml = document.getElementById('main');
+//   const detailsId = `details-${character.name}`;
+//   const detailsHtml = document.getElementById(detailsId);
+
+//   if (!detailsHtml) {
+//       const newDetails = document.createElement('div');
+//       newDetails.id = detailsId;
+//       newDetails.className = 'elixir-ingredients';
+//       newDetails.innerHTML = `
+//           <p>Altura: ${elixir}</p>`;
+
+//       button.parentNode.insertBefore(newDetails, button.nextSibling);
+//   } else {
+//       detailsHtml.parentNode.removeChild(detailsHtml);
+//   }
+// }
