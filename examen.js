e01efc752a7b2a48c4f223d1aba7c376b3c2884b
EvaCabrerizo <147143748+EvaCabrerizo@users.noreply.github.com>
2023-11-17 10:39:47 +0100

    ex

--- examen.js
+++ examen.js
@@ -6,43 +6,37 @@ window.onload = async() => {
   const spinnerHtmlElement = document.getElementById('spinner');
   spinnerHtmlElement.remove();
 
-  for (const person of persons) {
-      const mainHtmlElement = document.getElementById('main');
-      const newElement = document.createElement('div');
-      newElement.innerHTML = `
-        <h2>${person.firstName} ${person.lastName}</h2>
-        `;
 
-    }
-    mainHtmlElement.appendChild(newElement);
-  
-  };
+  for (const person of persons) {
+    const mainHtmlElement = document.getElementById('main');
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `
+      <h2>${person.firstName} ${person.lastName}</h2>`
+       for (const elixir of person.elixirs) {
+        
+        newElement.innerHTML += `
+           <p>${elixir.name}</p>
+           <button onclick="elixirClicat(${elixir.name})">Show ingredients of ${elixir.name}</button>
+         `;
+         
+       }
+
+       
+
+      
+     mainHtmlElement.appendChild(newElement);
+
+  }
+};
 
 async function getAllCharacters() {
   const response = await fetch(`${baseUrl}/Wizards`);
   const data = await response.json();
   return data;
 }
-async function getAllElixirs() {
+async function getAllIngredients(elixir) {
   const response = await fetch(`${baseUrl}/Elixirs`);
   const data = await response.json();
   return data;
 }
 
-// function showIngredients(elixir, button) {
-//   const mainHtml = document.getElementById('main');
-//   const detailsId = `details-${character.name}`;
-//   const detailsHtml = document.getElementById(detailsId);
-
-//   if (!detailsHtml) {
-//       const newDetails = document.createElement('div');
-//       newDetails.id = detailsId;
-//       newDetails.className = 'elixir-ingredients';
-//       newDetails.innerHTML = `
-//           <p>Altura: ${elixir}</p>`;
-
-//       button.parentNode.insertBefore(newDetails, button.nextSibling);
-//   } else {
-//       detailsHtml.parentNode.removeChild(detailsHtml);
-//   }
-// }
